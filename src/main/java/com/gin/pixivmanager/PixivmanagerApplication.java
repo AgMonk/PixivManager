9e40f9c9ceb58d5ee797b900ea4ed039de976f6b
ginstone <hjg719@139.com>
2020-09-07 11:38:53 +0800

    Initial commit

---
+++ src/main/java/com/gin/pixivmanager/PixivmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.gin.pixivmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixivmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixivmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gin/pixivmanager/PixivmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gin.pixivmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixivmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
